2855ee3733557585657bb138e74cc938116fe213
JuanM4T <plmauna@gmail.com>
2024-02-23 12:10:24 +0100

    Do not repeat the same character in a row (#3)
    Do not repeat the same character in a row
    
    ---------
    
    Co-authored-by: Agatem <agaatem@outlook.com>

--- js/script.js
+++ js/script.js
@@ -12,6 +12,7 @@ import {
 
 let sessionCounter = 0;
 let totalCounter = localStorage.getItem("totalCounter") || 0;
+let previousIndex = -1;
 
 const sessionCounterElement = document.getElementById("session-counter");
 const totalCounterElement = document.getElementById("total-counter");
@@ -108,7 +109,11 @@ function nextCharacter() {
   }
 
   const kanaArray = getKanaArray();
-  const charIndex = Math.floor(Math.random() * kanaArray.length);
+  let charIndex = Math.floor(Math.random() * kanaArray.length);
+  while (charIndex === previousIndex) {
+    charIndex = Math.floor(Math.random() * kanaArray.length);
+  }
+  previousIndex = charIndex;
   const randomCharacter = kanaArray[charIndex];
   const correctRomaji = romajiArray[charIndex];
 
@@ -117,7 +122,7 @@ function nextCharacter() {
   const incorrectAnswers = [];
   while (incorrectAnswers.length < 9) {
     const randomIncorrectIndex = Math.floor(
-      Math.random() * standardRomaji.length
+      Math.random() * standardRomaji.length,
     );
     const randomIncorrectRomaji = standardRomaji[randomIncorrectIndex];
 
