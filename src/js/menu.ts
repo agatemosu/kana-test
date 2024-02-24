1a1f1bb79debec4754e4889629e0f51472d4e4b4
Agatem <agaatem@outlook.com>
2024-02-24 14:07:25 +0100

    Fix TypeScript errors in menu.ts

--- src/js/menu.ts
+++ src/js/menu.ts
@@ -3,13 +3,16 @@ const arrow = document.getElementById("arrow");
 
 document.addEventListener("click", function (event) {
   const isClickInsideMenu =
-    menu.contains(event.target) || arrow.contains(event.target);
+    menu?.contains(event.target as Node) ||
+    arrow?.contains(event.target as Node);
 
-  if (!isClickInsideMenu) {
+  if (!isClickInsideMenu && menu) {
     menu.style.display = "none";
   }
 });
 
-arrow.addEventListener("click", function () {
-  menu.style.display = "grid";
+arrow?.addEventListener("click", function () {
+  if (menu) {
+    menu.style.display = "grid";
+  }
 });
